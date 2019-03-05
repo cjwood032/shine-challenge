f35d5152e36b291bc6ffb06a15e8a16e9278b2c6
cjwood032 <cjwood032@gmail.com>
2019-03-05 04:26:31 -0500

    grid of images created

--- src/App.js
+++ src/App.js
@@ -6,20 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="grid-container" cols="2">
+          <img src="https://via.placeholder.com/100?text=image0" className="images" />
+          <img src="https://via.placeholder.com/100?text=image1" className="images" />
+          <img src="https://via.placeholder.com/100?text=image2" className="images" />
+          <img src="https://via.placeholder.com/100?text=image3" className="images" />
+          <img src="https://via.placeholder.com/100?text=image4" className="images" />
+          <img src="https://via.placeholder.com/100?text=image5" className="images" />
+          <img src="https://via.placeholder.com/100?text=image6" className="images" />
+          <img src="https://via.placeholder.com/100?text=image7" className="images" />
+        </div>
       </div>
     );
   }
