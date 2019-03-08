9cf218666b19b1a6397a45fc2d5ffe94b666da04
cjwood032 <cjwood032@gmail.com>
2019-03-08 14:51:45 -0500

    onclick function increases count and adds to list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';   
 import './App.css';
-
+import imageData from './data.js'
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageList: []
+    }
+    this.renderImage=this.renderImage.bind(this)
+  
+  };
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    // add event listener to save state to localStorage
+    // when user leaves/refreshes the page
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+
+    // saves if component has a chance to unmount
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
+  renderImage(img) {
+    return(
+      <div id={img.id} onClick={(e) => this.clickCount(e, img)}>
+        <img src={img.src} key={img.id} />
+      </div>
+    )
+  }
+  clickCount= (e, img)=> {
+    e.preventDefault()
+    console.log(img.id +" clicked")
+    //debugger
+    if(!this.state.imageList.includes(img.id)){this.state.imageList.push(img)}
+    console.log(this.state.imageList)
+  }
+  clearList = (e) => {
+    e.preventDefault()
+    this.state.imageList=[]
+    console.log(this.state.imageList)
+  }
   render() {
     return (
       <div className="App">
-        <div class="grid-container" cols="2">
-          <img src="https://via.placeholder.com/100?text=image0" className="images" />
-          <img src="https://via.placeholder.com/100?text=image1" className="images" />
-          <img src="https://via.placeholder.com/100?text=image2" className="images" />
-          <img src="https://via.placeholder.com/100?text=image3" className="images" />
-          <img src="https://via.placeholder.com/100?text=image4" className="images" />
-          <img src="https://via.placeholder.com/100?text=image5" className="images" />
-          <img src="https://via.placeholder.com/100?text=image6" className="images" />
-          <img src="https://via.placeholder.com/100?text=image7" className="images" />
+      <button onClick= {(e) =>this.clearList(e)}>Clear the Order</button>
+        <div className="grid-container" cols="2">
+          {imageData.map(this.renderImage)}
         </div>
       </div>
     );
---
+++ src/data.js
@@ -0,0 +1,45 @@
+const imageData = [
+    {
+        src : `https://via.placeholder.com/100?text=image0`,
+        id :0,
+        clicks : 0
+    },
+    {
+        src : "https://via.placeholder.com/100?text=image1",
+          id : 1,
+          clicks : 0
+    },
+    {
+        src : "https://via.placeholder.com/100?text=image2",
+          id : 2,
+          clicks : 0
+    },
+    {
+        src : "https://via.placeholder.com/100?text=image3",
+          id : 3,
+          clicks : 0
+    },
+     {
+        src : "https://via.placeholder.com/100?text=image4",
+          id : 4,
+          clicks : 0
+    },
+    {
+        src : "https://via.placeholder.com/100?text=image5",
+          id : 5,
+          clicks : 0
+    },
+    {
+        src : "https://via.placeholder.com/100?text=image6",
+          id : 6,
+          clicks : 0
+    },
+    {
+        src : "https://via.placeholder.com/100?text=image7",
+          id : 7,
+          clicks : 0
+    },
+  ]
+  
+  export default imageData
+  
